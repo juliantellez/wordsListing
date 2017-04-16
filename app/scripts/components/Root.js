4594f09286ff1060b2bc2988bcb53b38889af42b
julian <juliantellezmendez@gmail.com>
2017-04-17 00:33:13 +0100

    .

--- app/scripts/components/Root.js
+++ app/scripts/components/Root.js
@@ -1,10 +1,28 @@
 import React from 'react'
 
+import Header from './sections/Header'
+import Content from './sections/Content'
+import Footer from './sections/Footer'
+
 export default class Root extends React.Component {
+  static childContextTypes = {
+    store: React.PropTypes.object.isRequired,
+    actions: React.PropTypes.object.isRequired,
+  }
+
+  getChildContext () {
+    return {
+      store,
+      actions,
+    }
+  }
+
   render () {
     return (
       <div className='Root'>
-        MAIN compo
+        <Header />
+        <Content />
+        <Footer />
       </div>
     )
   }
