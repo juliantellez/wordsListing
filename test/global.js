24842034b4ca229d433613dac6cdc3bb3524d323
julian <juliantellezmendez@gmail.com>
2017-04-17 01:18:39 +0100

    .

--- test/global.js
+++ test/global.js
@@ -4,7 +4,7 @@ import React from 'react'
 import sinonChai from 'sinon-chai'
 import chaiReact from 'chai-react'
 import {mount, shallow, render} from 'enzyme'
-import TestUtils from 'react-addons-test-utils'
+import TestUtils from 'react-dom/test-utils'
 
 import _ from 'lodash'
 
